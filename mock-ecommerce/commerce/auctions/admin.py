b658b28f98259c031b5f547f422e905b97464eea
dariangarcia-404 <58272481+dariangarcia-404@users.noreply.github.com>
2022-01-05 12:24:49 -0500

    rem dups

--- mock-ecommerce/commerce/auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(User)
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Comment)
